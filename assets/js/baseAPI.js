47c4773f6d16c745c12ae603592fc4eca7318e6d
itchihua <2049724486@qq.com>
2021-01-09 12:37:26 +0800

    更新了接口

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -3,7 +3,7 @@
 // 在这个函数中可以拿到我们给ajax提供的配置对象
 $.ajaxPrefilter(function(options){
     // 在发起真正的ajax之前 统一拼接请求的根路径
-    options.url = 'http://ajax.frontend.itheima.net' + options.url
+    options.url = 'http://127.0.0.1:3007' + options.url
     // console.log(options.url);
 
     // 统一为有权限的接口，设置 headers 请求头
