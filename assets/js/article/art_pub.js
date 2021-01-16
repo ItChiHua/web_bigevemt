d32e50978ec9a8f2d6af8e360f3f00af6fe9ebeb
itchihua <2049724486@qq.com>
2021-01-16 19:18:30 +0800

    updata

--- assets/js/article/art_pub.js
+++ assets/js/article/art_pub.js
@@ -135,7 +135,7 @@ $(function(){
                 }
                 layer.msg('发布文章成功！')
                 // 发布文章成功后 跳转到文章列表页面
-                location.href = '../article/art_list.html'
+                location.href = './art_list.html'
             }
         })
     }
